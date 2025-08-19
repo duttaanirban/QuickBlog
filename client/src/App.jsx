32c9496afde069a3f39eae28175d1caa0bb3daa3
duttaanirban <duttaanirbanad@gmail.com>
2025-08-19 21:35:07 +0530

    page creations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-700">
-        Hello world!
-      </h1>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/blog" element={<BlogPage />} />
+    </Routes>
+    </>
   )
 }
 
