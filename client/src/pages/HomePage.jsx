6de2b5c39a2b2b265f5c50062c7305e19ea918e6
duttaanirban <duttaanirbanad@gmail.com>
2025-08-20 20:49:56 +0530

    newsletter created

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import Bloglist from '../components/Bloglist.jsx'
 import Header from '../components/Header.jsx'
 import Navbar from '../components/Navbar.jsx'
+import Newsletter from '../components/Newsletter.jsx'
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <Navbar />
       <Header />
       <Bloglist />
+      <Newsletter />
     </>
   )
 }
