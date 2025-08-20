679dfeea15c3bf3ffee697fc9e622f45370d70cc
duttaanirban <duttaanirbanad@gmail.com>
2025-08-20 19:32:52 +0530

    created header section for homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import Header from '../components/Header.jsx'
 import Navbar from '../components/Navbar.jsx'
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 }
