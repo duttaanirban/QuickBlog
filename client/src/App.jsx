63b97fc74070ecf0557e7256f3a19ae34ff111a6
duttaanirban <duttaanirbanad@gmail.com>
2025-08-23 20:45:17 +0530

    admin route created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import BlogPage from './pages/BlogPage.jsx'
+import Layout from './pages/admin/Layout.jsx'
+import Dashboard from './pages/admin/dashboard.jsx'
+import AddBlog from './pages/admin/AddBlog.jsx'
+import ListBlog from './pages/admin/ListBlog.jsx'
+import Comments from './pages/admin/Comments.jsx'
 
 const App = () => {
   return (
@@ -8,6 +13,12 @@ const App = () => {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/blog/:id" element={<BlogPage />} />
+      <Route path="/admin" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path='addBlog' element={<AddBlog />} />
+        <Route path='listBlog' element={<ListBlog />} />
+        <Route path='comments' element={<Comments />} />
+      </Route>
     </Routes>
     </>
   )
