844c0d0e6e29886e3fc8b6598a91dd3fd302a0d3
duttaanirban <duttaanirbanad@gmail.com>
2025-08-26 00:26:25 +0530

    addblog

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,18 +1,38 @@
+import fs from "fs";
+import imagekit from "../configs/imageKit.js";
+
 export const addBlog = async (req, res) => {
     try {
         const { title, subTitle, category, image, isPublished, description } = JSON.parse(req.body.blog);
 
-        const newBlog = new Blog({
-            title,
-            subTitle,
-            category,
-            image,
-            isPublished,
-            description
+        const imageFile = req.file;
+        
+        if(!title || !category || !imageFile || !description) {
+            return res.status(400).json({ success: false, message: "Title, Category, Image and Description are required" });
+        }
+
+        const fileBuffer = fs.readFileSync(imageFile.path);
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: "/blogs"
         });
 
-        await newBlog.save();
-        res.status(201).json({success: true, message: "Blog added successfully", blog: newBlog });
+        // optimize image
+        const optimizedImage = await imagekit.url({
+            path: response.filePath,
+            transformation: [{
+                quality: 'auto',
+                width: '1280',
+                format: 'webp',
+            }]
+        });
+
+        const img = optimizedImage;
+
+        await Blog.create({title, subTitle, category, image: img, isPublished, description});
+
+        res.status(201).json({ success: true, message: "Blog added successfully" });
     } catch (error) {
         res.status(500).json({ success: false, message: "Error adding blog", error: error.message });
     }
