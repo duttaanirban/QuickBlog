75f2fbd8e20744dfad7948808bb494f598776f1b
duttaanirban <duttaanirbanad@gmail.com>
2025-08-26 21:31:27 +0530

    api endpoints created

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,13 +1,14 @@
 import e from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, togglePublishBlog } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
 
 const blogRouter = e.Router();
 
 blogRouter.post("/add", upload.single("image"), auth, addBlog);
-
-
-
+blogRouter.get("/all", auth, getAllBlogs);
+blogRouter.get("/:blogId", auth, getBlogById);
+blogRouter.post("/delete", auth, deleteBlogById);
+blogRouter.post("/toggle-publish", auth, togglePublishBlog);
 
 export default blogRouter;
