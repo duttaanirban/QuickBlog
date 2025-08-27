42151996ae6ba36609bfdb8004482daf2a6b5e25
duttaanirban <duttaanirbanad@gmail.com>
2025-08-27 11:45:34 +0530

    login functionality added

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import emailIcon from "../../assets/email_icon.png";
 import userIcon from "../../assets/user_icon.svg";
-
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const Login = () => {
+
+  const {axios, setToken} = useAppContext();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/admin/login", { email, password });
+      if (data.success) {
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error("Error logging in");
+      console.error("Error logging in:", error);
+    }
   }
 
   return (
