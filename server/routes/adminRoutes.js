db37f3a75ab40653db0e07eef11790fe7b17ea32
duttaanirban <duttaanirbanad@gmail.com>
2025-08-27 10:27:49 +0530

    admin routes updated

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { adminLogin } from "../controllers/adminController.js";
+import { adminLogin, approveCommentById,
+    deleteCommentById, getAllBlogsAdmin,
+    getAllComments, getDashboardStats }
+    from "../controllers/adminController.js";
+import auth from "../middleware/auth.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", adminLogin);
+adminRouter.get("/blogs", auth, getAllBlogsAdmin);
+adminRouter.get("/comments", auth, getAllComments);
+adminRouter.get("/dashboard", auth, getDashboardStats);
+adminRouter.delete("/comments/:id", auth, deleteCommentById);
+adminRouter.put("/comments/:id/approve", auth, approveCommentById);
 
 export default adminRouter;
 
