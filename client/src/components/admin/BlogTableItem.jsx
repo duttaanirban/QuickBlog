87305617725d6eaa5d71f6b850ed7d7f9b0b05a7
duttaanirban <duttaanirbanad@gmail.com>
2025-08-29 00:47:19 +0530

    list blogs updated

--- client/src/components/admin/BlogTableItem.jsx
+++ client/src/components/admin/BlogTableItem.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const BlogTableItem = ({blog, fetchBlogs, index}) => {
 
 
     const {title, createdAt} = blog;
     const blogDate = new Date(createdAt);
+
+    const {axios} = useAppContext();
+
+    const deleteBlog = async () => {
+      const confirm = window.confirm("Are you sure you want to delete this blog?");
+      if (!confirm) return;
+      try {
+        const {data} = await axios.post(`/api/blog/delete`, {id: blog._id});
+        if (data.success) {
+          toast.success("Blog deleted successfully!");
+          await fetchBlogs();
+        } else {
+          toast.error("Error deleting blog");
+        }
+      } catch (error) {
+        toast.error("Error deleting blog");
+        console.error("Error deleting blog:", error.message);
+      }
+    }
+
+    const togglePublish = async () => {
+      try {
+        const {data} = await axios.post(`/api/blog/toggle-publish`, {id: blog._id});
+        if (data.success) {
+          toast.success("Blog updated successfully!");
+          await fetchBlogs();
+        } else {
+          toast.error("Error updating blog");
+        }
+      } catch (error) {
+        toast.error("Error updating blog");
+        console.error("Error updating blog:", error.message);
+      }
+    }
 
   return (
     <tr className='border-y border-gray-300'>
@@ -16,10 +52,10 @@ const BlogTableItem = ({blog, fetchBlogs, index}) => {
             <p className={`${blog.isPublished ? 'text-green-600' : 'text-orange-700'}`}>{blog.isPublished ? 'Published' : 'Unpublished'}</p>
         </td>
         <td className='px-2 py-4 flex text-xs gap-3'>
-          <button className="border px-2 py-0.5 mt-1 rounded cursor-pointer" onClick={() => fetchBlogs(blog.id)}>
+          <button onClick={togglePublish} className="border px-2 py-0.5 mt-1 rounded cursor-pointer">
             {blog.isPublished ? 'Unpublished' : 'Published'}
           </button>
-          <img src={assets.cross_icon} alt='' className='w-8 hover:scale-110 transition-all cursor-pointer'/>
+          <img onClick={deleteBlog} src={assets.cross_icon} alt='' className='w-8 hover:scale-110 transition-all cursor-pointer'/>
         </td>
         
     </tr>
